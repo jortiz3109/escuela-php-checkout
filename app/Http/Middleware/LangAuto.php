749cfca51be27a70917a84834b9c341e53ef4e8a
Arik <arik.pena@sabi.com.co>
2021-10-02 14:55:51 -0500

    feat: add middleware for change language

---
+++ app/Http/Middleware/LangAuto.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class LangAuto
+{
+    const LOCALES = ['en', 'es'];
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+
+    public function handle(Request $request, Closure $next)
+    {
+        app()->setLocale($request->getPreferredLanguage(self::LOCALES));
+
+        return $next($request);
+    }
+}
