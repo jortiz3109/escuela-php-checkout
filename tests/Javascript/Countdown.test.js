5305bb1e8c2e10568ff98837662831c022fdc702
Lauana <lauanahubler@gmail.com>
2021-10-18 17:21:17 -0500

    style: js tests indentation

--- tests/Javascript/Countdown.test.js
+++ tests/Javascript/Countdown.test.js
@@ -1,70 +1,70 @@
-import Countdown from "../../resources/js/components/Countdown";
+import Countdown from '../../resources/js/components/Countdown';
 import { mount } from '@vue/test-utils';
-import { DateTime } from "luxon";
+import { DateTime } from 'luxon';
 
 describe('Countdown', () => {
-    let wrapper, clock;
+	let wrapper, clock;
 
-    beforeEach(() => {
-        clock = jest.useFakeTimers();
-        wrapper = mount(Countdown, {
-            props: {
-                expiration: DateTime.now().plus({minutes: 15}).toISO()
-            }
-        });
-    })
+	beforeEach(() => {
+		clock = jest.useFakeTimers();
+		wrapper = mount(Countdown, {
+			props: {
+				expiration: DateTime.now().plus({minutes: 15}).toISO()
+			}
+		});
+	});
 
-    afterEach(() =>  jest.useRealTimers());
+	afterEach(() =>  jest.useRealTimers());
 
-    test('it renders a countdown timer', () => {
-        see('00:15:00');
-    })
+	test('it renders a countdown timer', () => {
+		see('00:15:00');
+	});
 
-    test('it reduces the countdown every second', () => {
-        see('00:15:00');
+	test('it reduces the countdown every second', () => {
+		see('00:15:00');
 
-        clock.advanceTimersByTime(1000);
+		clock.advanceTimersByTime(1000);
 
-        wrapper.vm.$nextTick(() => {
-            see('00:14:59');
-        });
-    })
+		wrapper.vm.$nextTick(() => {
+			see('00:14:59');
+		});
+	});
 
-    test('it stops countdown when gets to zero', () => {
-        see('00:15:00');
+	test('it stops countdown when gets to zero', () => {
+		see('00:15:00');
 
-        clock.advanceTimersByTime(900000); // 15 minutes
+		clock.advanceTimersByTime(900000); // 15 minutes
 
-        wrapper.vm.$nextTick(() => {
-            see('00:00:00');
-            expect(wrapper.vm.intervalHandle).toBeUndefined()
-        });
-    })
+		wrapper.vm.$nextTick(() => {
+			see('00:00:00');
+			expect(wrapper.vm.intervalHandle).toBeUndefined();
+		});
+	});
 
-    test('it shows zero if session is expired', () => {
-        wrapper = mount(Countdown, {
-            props: {
-                expiration: DateTime.now().plus({minutes: -15}).toISO()
-            }
-        });
+	test('it shows zero if session is expired', () => {
+		wrapper = mount(Countdown, {
+			props: {
+				expiration: DateTime.now().plus({minutes: -15}).toISO()
+			}
+		});
 
-        see('00:00:00');
-    })
+		see('00:00:00');
+	});
 
-    test('it broadcasts when session is expired', () => {
-        see('00:15:00');
+	test('it broadcasts when session is expired', () => {
+		see('00:15:00');
 
-        clock.advanceTimersByTime(900000); // 15 minutes
+		clock.advanceTimersByTime(900000); // 15 minutes
 
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.emitted().expired).toBeTruthy()
-        });
-    })
+		wrapper.vm.$nextTick(() => {
+			expect(wrapper.emitted().expired).toBeTruthy();
+		});
+	});
 
-    // Helper functions
-    let see = (text, selector) => {
-        let wrap = selector ? wrapper.find(selector) : wrapper;
+	// Helper functions
+	let see = (text, selector) => {
+		let wrap = selector ? wrapper.find(selector) : wrapper;
 
-        expect(wrap.html()).toContain(text);
-    }
+		expect(wrap.html()).toContain(text);
+	};
 });
--- tests/Javascript/PayerData.test.js
+++ tests/Javascript/PayerData.test.js
@@ -1,217 +1,217 @@
-import PayerData from "../../resources/js/components/PayerData"
+import PayerData from '../../resources/js/components/PayerData';
 import { mount, flushPromises } from '@vue/test-utils';
 
 describe('PayerData', () => {
-    let wrapper;
+	let wrapper;
 
-    beforeEach(() => {
-        jest.useFakeTimers()
-        wrapper = mount(PayerData);
-    })
+	beforeEach(() => {
+		jest.useFakeTimers();
+		wrapper = mount(PayerData);
+	});
 
-    afterEach(() =>  jest.useRealTimers());
+	afterEach(() =>  jest.useRealTimers());
 
-    test('it can set a name', async () => {
-        const input = wrapper.find('input[name=name]');
+	test('it can set a name', async () => {
+		const input = wrapper.find('input[name=name]');
 
-        await input.setValue('Julia');
+		await input.setValue('Julia');
 
-        expect(input.element.value).toBe('Julia')
-        dontSee('The field name is invalid')
-    })
+		expect(input.element.value).toBe('Julia');
+		dontSee('The field name is invalid');
+	});
 
-    test('it can validate name as required', async () => {
-        dontSee('The field name is invalid')
+	test('it can validate name as required', async () => {
+		dontSee('The field name is invalid');
 
-        const input = wrapper.find('input[name=name]');
+		const input = wrapper.find('input[name=name]');
 
-        await input.setValue('');
+		await input.setValue('');
 
-        jest.runAllTimers();
-        await flushPromises();
+		jest.runAllTimers();
+		await flushPromises();
 
-        see('The field name is invalid')
-    })
+		see('The field name is invalid');
+	});
 
-    test('it can validate name is too long', async () => {
-        dontSee('The field name is invalid')
+	test('it can validate name is too long', async () => {
+		dontSee('The field name is invalid');
 
-        const input = wrapper.find('input[name=name]');
+		const input = wrapper.find('input[name=name]');
 
-        await input.setValue('a'.repeat(81));
+		await input.setValue('a'.repeat(81));
 
-        jest.runAllTimers();
-        await flushPromises();
+		jest.runAllTimers();
+		await flushPromises();
 
-        see('The field name is invalid')
-    })
+		see('The field name is invalid');
+	});
 
-    test('it can set a surname', async () => {
-        const input = wrapper.find('input[name=surname]');
+	test('it can set a surname', async () => {
+		const input = wrapper.find('input[name=surname]');
 
-        await input.setValue('Costa');
+		await input.setValue('Costa');
 
-        expect(input.element.value).toBe('Costa')
-        dontSee('The field surname is invalid')
-    })
+		expect(input.element.value).toBe('Costa');
+		dontSee('The field surname is invalid');
+	});
 
-    test('it can validate surname as required', async () => {
-        dontSee('The field surname is invalid')
+	test('it can validate surname as required', async () => {
+		dontSee('The field surname is invalid');
 
-        const input = wrapper.find('input[name=surname]');
+		const input = wrapper.find('input[name=surname]');
 
-        await input.setValue('');
+		await input.setValue('');
 
-        jest.runAllTimers();
-        await flushPromises();
+		jest.runAllTimers();
+		await flushPromises();
 
-        see('The field surname is invalid')
-    })
+		see('The field surname is invalid');
+	});
 
-    test('it can validate surname is too long', async () => {
-        dontSee('The field surname is invalid')
+	test('it can validate surname is too long', async () => {
+		dontSee('The field surname is invalid');
 
-        const input = wrapper.find('input[name=surname]');
+		const input = wrapper.find('input[name=surname]');
 
-        await input.setValue('a'.repeat(81));
+		await input.setValue('a'.repeat(81));
 
-        jest.runAllTimers();
-        await flushPromises();
+		jest.runAllTimers();
+		await flushPromises();
 
-        see('The field surname is invalid')
-    })
+		see('The field surname is invalid');
+	});
 
-    test('it can select a document type', async () => {
-        const select = wrapper.find('select[name=documentType]');
+	test('it can select a document type', async () => {
+		const select = wrapper.find('select[name=documentType]');
 
-        await select.setValue('CC');
+		await select.setValue('CC');
 
-        expect(select.element.value).toBe('CC')
-    })
+		expect(select.element.value).toBe('CC');
+	});
 
-    test('it can validate document type as required', async () => {
-        const select = wrapper.find('select[name=documentType]');
+	test('it can validate document type as required', async () => {
+		const select = wrapper.find('select[name=documentType]');
 
-        expect(select.classes()).not.toContain('border-red-500')
+		expect(select.classes()).not.toContain('border-red-500');
 
-        await select.setValue('');
+		await select.setValue('');
 
-        jest.runAllTimers();
-        await flushPromises();
+		jest.runAllTimers();
+		await flushPromises();
 
-        expect(select.classes()).toContain('border-red-500')
-    })
+		expect(select.classes()).toContain('border-red-500');
+	});
 
-    test('it can set a document', async () => {
-        const input = wrapper.find('input[name=document]');
+	test('it can set a document', async () => {
+		const input = wrapper.find('input[name=document]');
 
-        await input.setValue('FR725498');
+		await input.setValue('FR725498');
 
-        expect(input.element.value).toBe('FR725498')
-        dontSee('The field document is invalid')
-    })
+		expect(input.element.value).toBe('FR725498');
+		dontSee('The field document is invalid');
+	});
 
-    test('it can validate document as required', async () => {
-        dontSee('The field document is invalid')
+	test('it can validate document as required', async () => {
+		dontSee('The field document is invalid');
 
-        const input = wrapper.find('input[name=document]');
+		const input = wrapper.find('input[name=document]');
 
-        await input.setValue('');
+		await input.setValue('');
 
-        jest.runAllTimers();
-        await flushPromises();
+		jest.runAllTimers();
+		await flushPromises();
 
-        see('The field document is invalid')
-    })
+		see('The field document is invalid');
+	});
 
-    test('it can set a email', async () => {
-        const input = wrapper.find('input[name=email]');
+	test('it can set a email', async () => {
+		const input = wrapper.find('input[name=email]');
 
-        await input.setValue('admin@gmail.com');
+		await input.setValue('admin@gmail.com');
 
-        expect(input.element.value).toBe('admin@gmail.com')
-        dontSee('The field email is invalid')
-    })
+		expect(input.element.value).toBe('admin@gmail.com');
+		dontSee('The field email is invalid');
+	});
 
-    test('it can validate email as required', async () => {
-        dontSee('The field email is invalid')
+	test('it can validate email as required', async () => {
+		dontSee('The field email is invalid');
 
-        const input = wrapper.find('input[name=email]');
+		const input = wrapper.find('input[name=email]');
 
-        await input.setValue('');
+		await input.setValue('');
 
-        jest.runAllTimers();
-        await flushPromises();
+		jest.runAllTimers();
+		await flushPromises();
 
-        see('The field email is invalid')
-    })
+		see('The field email is invalid');
+	});
 
-    test('it can validate email as valid email', async () => {
-        dontSee('The field email is invalid')
+	test('it can validate email as valid email', async () => {
+		dontSee('The field email is invalid');
 
-        const input = wrapper.find('input[name=email]');
+		const input = wrapper.find('input[name=email]');
 
-        await input.setValue('admin');
+		await input.setValue('admin');
 
-        jest.runAllTimers();
-        await flushPromises();
+		jest.runAllTimers();
+		await flushPromises();
 
-        see('The field email is invalid')
-    })
+		see('The field email is invalid');
+	});
 
-    test('it can set a mobile', async () => {
-        const input = wrapper.find('input[name=mobile]');
+	test('it can set a mobile', async () => {
+		const input = wrapper.find('input[name=mobile]');
 
-        await input.setValue('3008923456');
+		await input.setValue('3008923456');
 
-        expect(input.element.value).toBe('3008923456')
-        dontSee('The field mobile is invalid')
-    })
+		expect(input.element.value).toBe('3008923456');
+		dontSee('The field mobile is invalid');
+	});
 
-    test('it can validate mobile as required', async () => {
-        dontSee('The field mobile is invalid')
+	test('it can validate mobile as required', async () => {
+		dontSee('The field mobile is invalid');
 
-        const input = wrapper.find('input[name=mobile]');
+		const input = wrapper.find('input[name=mobile]');
 
-        await input.setValue('');
+		await input.setValue('');
 
-        jest.runAllTimers();
-        await flushPromises();
+		jest.runAllTimers();
+		await flushPromises();
 
-        see('The field mobile is invalid')
-    })
+		see('The field mobile is invalid');
+	});
 
-    test.skip('it broadcasts when submit correctly', async () => {
-        await wrapper.find('input[name=name]').setValue('Julia')
-        await wrapper.find('input[name=surname]').setValue('Costa')
-        await wrapper.find('select[name=documentType]').setValue('CC')
-        await wrapper.find('input[name=document]').setValue('1092823456')
-        await wrapper.find('input[name=email]').setValue('julia.costa@gmail.com')
-        await wrapper.find('input[name=mobile]').setValue('3009283456')
+	test.skip('it broadcasts when submit correctly', async () => {
+		await wrapper.find('input[name=name]').setValue('Julia');
+		await wrapper.find('input[name=surname]').setValue('Costa');
+		await wrapper.find('select[name=documentType]').setValue('CC');
+		await wrapper.find('input[name=document]').setValue('1092823456');
+		await wrapper.find('input[name=email]').setValue('julia.costa@gmail.com');
+		await wrapper.find('input[name=mobile]').setValue('3009283456');
 
-        await wrapper.find('button[type=submit]').trigger('click')
+		await wrapper.find('button[type=submit]').trigger('click');
 
-        expect(wrapper.emitted()).toHaveProperty('save-payer')
-        expect(wrapper.emitted('save-payer')[0]).toEqual({
-            name: 'Julia',
-            surname: 'Costa',
-            documentType: 'CC',
-            document: '1092823456',
-            email: 'julia.costa@gmail.com',
-            mobile: '3009283456',
-        })
-    })
+		expect(wrapper.emitted()).toHaveProperty('save-payer');
+		expect(wrapper.emitted('save-payer')[0]).toEqual({
+			name: 'Julia',
+			surname: 'Costa',
+			documentType: 'CC',
+			document: '1092823456',
+			email: 'julia.costa@gmail.com',
+			mobile: '3009283456',
+		});
+	});
 
-    // Helper functions
-    let see = (text, selector) => {
-        let wrap = selector ? wrapper.find(selector) : wrapper;
+	// Helper functions
+	let see = (text, selector) => {
+		let wrap = selector ? wrapper.find(selector) : wrapper;
 
-        expect(wrap.html()).toContain(text);
-    }
+		expect(wrap.html()).toContain(text);
+	};
 
-    let dontSee = (text, selector) => {
-        let wrap = selector ? wrapper.find(selector) : wrapper;
+	let dontSee = (text, selector) => {
+		let wrap = selector ? wrapper.find(selector) : wrapper;
 
-        expect(wrap.html()).not.toContain(text);
-    }
+		expect(wrap.html()).not.toContain(text);
+	};
 });
