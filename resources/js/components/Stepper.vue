793963d2cb009a4347e62aa9f6911cd0d1361c52
Lauana <lauanahubler@gmail.com>
2021-10-20 10:30:09 -0500

    refactor: stepper bar

--- resources/js/components/Stepper.vue
+++ resources/js/components/Stepper.vue
@@ -1,43 +1,42 @@
 <template>
   <div class="flex flex-col w-1/5 items-center">
-    <div class="flex relative justify-between w-full">
-      <div class="flex flex-col items-center z-10">
-        <div class="step-circle border-blue-900">
-          1
-        </div>
-        <p class="step-text">
-          Payer data
-        </p>
+    <div class="flex relative justify-between w-5/6 self-center">
+      <div class="step-circle border-blue-900">
+        1
       </div>
-      <div class="flex flex-col items-center z-10">
-        <div
-          class="step-circle"
-          :class="step < 2 ? 'border-blue-100' : 'border-blue-900'"
-        >
-          2
-        </div>
-        <p class="step-text">
-          Select payment method
-        </p>
+
+      <div
+        class="step-circle"
+        :class="step < 2 ? 'border-blue-100' : 'border-blue-900'"
+      >
+        2
       </div>
-      <div class="flex flex-col items-center z-10">
-        <div
-          class="step-circle"
-          :class="step < 3 ? 'border-blue-100' : 'border-blue-900'"
-        >
-          3
-        </div>
-        <p class="step-text">
-          Card data
-        </p>
+
+      <div
+        class="step-circle"
+        :class="step < 3 ? 'border-blue-100' : 'border-blue-900'"
+      >
+        3
       </div>
-      <div class="absolute w-5/6 self-center left-4 top-1/4">
+
+      <div class="absolute w-full self-center">
         <div class="bar bg-blue-100 w-full" />
         <div
           class="bar bg-blue-900 transition-bar"
           :class="barLength()"
         />
       </div>
+    </div>
+    <div class="flex relative justify-between w-full">
+      <p class="step-text">
+        Fill in payer data
+      </p>
+      <p class="step-text">
+        Select payment method
+      </p>
+      <p class="step-text">
+        Insert card data
+      </p>
     </div>
   </div>
 </template>
@@ -48,8 +47,8 @@ import useStep from '../functions/useStep';
 export default {
 	name: 'Stepper',
 
-	setup () {
-		const { step } = useStep();
+	setup() {
+		const {step} = useStep();
 
 		function barLength() {
 			const lengths = {
@@ -58,29 +57,29 @@ export default {
 				step3: 'w-full'
 			};
 
-			return lengths['step'+step.value];
+			return lengths['step' + step.value];
 		}
 
-		return { step, barLength };
+		return {step, barLength};
 	}
 };
 </script>
 
 <style scoped>
-    .step-circle {
-        @apply flex bg-white border-4 h-10 w-10 rounded-full font-bold justify-center items-center shadow;
-        transition: border-color 0.5s ease-in-out 0.1s;
-    }
+.step-circle {
+    @apply flex bg-white border-4 h-10 w-10 rounded-full font-bold justify-center items-center shadow z-10;
+    transition: border-color 0.5s ease-in-out 0.1s;
+}
 
-    .step-text {
-        @apply text-center text-sm;
-    }
+.step-text {
+    @apply text-center text-sm;
+}
 
-    .bar {
-        @apply absolute self-center h-1.5 rounded-full z-0 shadow;
-    }
+.bar {
+    @apply absolute self-center h-1.5 rounded-full z-0 shadow;
+}
 
-    .transition-bar {
-        transition: width 0.5s ease-out;
-    }
+.transition-bar {
+    transition: width 0.5s ease-out;
+}
 </style>
