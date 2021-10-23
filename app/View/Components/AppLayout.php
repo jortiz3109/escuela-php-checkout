656f0d9581652cfd86d2fbfcfa8be4805e786ccc
Lauana <lauanahubler@gmail.com>
2021-10-23 11:52:31 -0500

    style: removed unnecessary comment

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -7,11 +7,6 @@ use Illuminate\View\View;
 
 class AppLayout extends Component
 {
-    /**
-     * Get the view / contents that represents the component.
-     *
-     * @return View
-     */
     public function render(): View
     {
         return view('layouts.app');
