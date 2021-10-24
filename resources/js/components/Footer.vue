58082930c301ec465f50ed76444e9e3947d71c54
Lauana <lauanahubler@gmail.com>
2021-10-24 10:28:22 -0500

    refactor: adjust useStep function

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="grid grid-cols-2 items-baseline w-5/6">
     <button
-      v-if="step > 1"
+      v-if="step > firstStep"
       class="bg-gray-500 text-white shadow py-3 px-12 my-8 rounded-lg justify-self-start hover:bg-gray-700"
       @click="stepBack"
     >
       Back
     </button>
     <a
-      v-if="step < 4"
+      v-if="step < lastStep"
       href="#"
       class="p-2 my-8 justify-self-end col-start-2 underline text-gray-500 hover:text-blue-900"
     >Do not want to continue</a>
@@ -22,9 +22,9 @@ export default {
 	name: 'Footer',
 
 	setup() {
-		const { step, stepBack } = useStep();
+		const { step, firstStep, lastStep, stepBack } = useStep();
 
-		return { step, stepBack };
+		return { step, firstStep, lastStep, stepBack };
 	}
 };
 </script>
--- resources/js/functions/useStep.js
+++ resources/js/functions/useStep.js
@@ -1,28 +1,31 @@
 import { ref } from 'vue';
 
 const step = ref(1);
-const firstStep = 1;
-const lastStep = 4;
-
-function stepBack() {
-	if (step.value > firstStep) {
-		step.value--;
-	}
-}
-
-function stepForward() {
-	if (step.value < lastStep) {
-		step.value++;
-	}
-}
-
-function inStep(value) {
-	return step.value === value;
-}
 
 export default function () {
+    const firstStep = 1;
+    const lastStep = 4;
+
+    function stepBack() {
+        if (step.value > firstStep) {
+            step.value--;
+        }
+    }
+
+    function stepForward() {
+        if (step.value < lastStep) {
+            step.value++;
+        }
+    }
+
+    function inStep(value) {
+        return step.value === value;
+    }
+
 	return {
 		step,
+        firstStep,
+        lastStep,
 		stepBack,
 		stepForward,
 		inStep
