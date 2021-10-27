e6a758e4ca57b28c5c30d3dc196f843cac45a451
Juan David Zea <vikzea@gmail.com>
2021-10-27 11:20:27 -0500

    refactor: Move prefix from RouteServiceProvider to route group

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\Api\V1\PaymentMethodController;
 use Illuminate\Support\Facades\Route;
 
- Route::get('session/{session:uuid}/payment-methods', [PaymentMethodController::class, 'discover'])->name('api.v1.paymentMethods.discover');
+Route::prefix('v1')->group(function () {
+    Route::get('session/{session:uuid}/payment-methods', [PaymentMethodController::class, 'discover'])->name('api.v1.paymentMethods.discover');
+});
