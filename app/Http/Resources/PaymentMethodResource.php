0ee4517dd8469b7af83bcea6a3499759f70f1103
Juan David Zea <vikzea@gmail.com>
2021-10-26 22:12:33 -0500

    refactor: Removal of comments that do not add value

--- app/Http/Resources/PaymentMethodResource.php
+++ app/Http/Resources/PaymentMethodResource.php
@@ -2,17 +2,10 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PaymentMethodResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  Request  $request
-     * @return array
-     */
     public function toArray($request)
     {
         return [
